f16b8f9c5e94df791428540ddfe6cea32f607675
Bee <thammawat@muze.co.th>
2025-06-19 08:50:10 +0700

    set up process.env

--- src/configs/postgres.database.config.ts
+++ src/configs/postgres.database.config.ts
@@ -4,15 +4,14 @@ import { Logger } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-
 export const postgresDbConfig = (): PostgresConnectionOptions => ({
   type: 'postgres',
-  host: 'localhost',
-  port: 54325,
-  username: 'core',
-  password: 'password',
-  database: 'jtket',
-  schema: 'jtket',
+  host: process.env.DB_HOST,
+  port: +(process.env.DB_PORT || 5432),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  schema: process.env.DB_SCHEMA,
   logging: false,
   entities: [join(__dirname, '../entities/*.entity{.ts,.js}')],
   migrations: [join(__dirname, '../migrations/postgres/*{.ts,.js}')],
